3a035e12ab2a31645db3ffb640bb9566d49c9c6e
Krisztian Korosi <koeroesi86@gmail.com>
2022-12-06 08:11:05 +0100

    Quick fix sanitiser

--- src/handlers/sanitise.ts
+++ src/handlers/sanitise.ts
@@ -1,11 +1,21 @@
 import moxios from 'moxios';
 
+type Req = ReturnType<typeof moxios.requests.at>;
+
+interface Ret {
+  baseUrl?: string;
+  url?: string;
+  method?: string;
+  headers?: Record<string, string | string[]>;
+  data: any;
+}
+
 export const sanitiseMoxios = (
-  request: ReturnType<typeof moxios.requests.at>
-) => ({
+  request: Req
+): Ret => ({
   baseUrl: request.config.baseURL,
   url: request.config.url,
   method: request.config.method,
-  headers: { ...request.config.headers },
+  headers: { ...request.config.headers } as Record<string, string | string[]>,
   data: request.config.data,
 });
